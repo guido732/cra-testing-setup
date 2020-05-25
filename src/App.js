8fe4852a23402d315031936c9fc3619d90f487c3
guido732 <guido732@gmail.com>
2020-05-25 16:03:59 -0300

    feat (app.js) integrates custom component to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { MyComponent } from "./MyComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [value, setValue] = React.useState("");
+	const handleChange = (e) => {
+		setValue(e.target.value);
+		console.log(e.target.value);
+	};
+	return (
+		<div>
+			<MyComponent
+				error={false}
+				success={false}
+				disabled={false}
+				size="sm"
+				value={value}
+				id="my-component"
+				name="myComponentDemo"
+				label="This is a label"
+				type="text"
+				className="my-component-class"
+				placeholder="Placeholder Text"
+				onChange={handleChange}
+			/>
+		</div>
+	);
 }
 
 export default App;
